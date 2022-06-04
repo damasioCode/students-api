5fff0fdb58f069cb3d9894f919e34f5d7cccdb9a
Daniel <damasio.dan7@gmail.com>
2022-06-04 20:26:43 -0300

    finish : deploy dev

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+
+    public function getAllStudents() {
+        // get all students
+        $students = Student::get()->toJson(JSON_PRETTY_PRINT);
+        return response($students, 200);
+    }
+
+    public function getStudent ( $id ) {
+        // get a student
+
+        if( Student::where('id', $id )->exists() ) {
+            $student = Student::where('id', $id )->get()->toJson(JSON_PRETTY_PRINT);
+            return response($student, 200);
+        } else {
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
+    }
+
+    public function createStudent( Request $request ) {
+        try {
+            $student = new Student();
+            $student->name = $request->name;
+            $student->course = $request->course;
+            $student->save();
+
+            return response()->json([
+                'message' => 'Student created successfully',
+            ]);
+            
+        } catch (\Exception $e ) {
+            return response()->json([
+                'message' => 'Error creating student',
+            ], 400);
+        }
+    }
+
+    public function updateStudent ( Request $request, $id ) 
+    {
+        // update a student
+        if( Student::where('id', $id)->exists() ) {
+            $student = Student::find($id);
+            $student->name = is_null($request->name) ? $student->name : $request->name;
+            $student->course = is_null($request->course) ? $student->course : $request->course;
+            $student->save();
+
+            return response()->json([
+                'message' => 'Student updated successfully'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
+    }
+
+    public function deleteStudent ( $id ) 
+    {
+        // delete a student
+        if( Student::where('id', $id)->exists() ) {
+            $student = Student::find($id);
+            $student->delete();
+
+            return response()->json([
+                'message' => 'Student delete successfully'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ApiController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('students', 'App\Http\Controllers\ApiController@getAllStudents');
+Route::get('students/{id}', 'App\Http\Controllers\ApiController@getStudent');
+Route::post('students/create', 'App\Http\Controllers\ApiController@createStudent');
+Route::put('students/update/{id}', 'App\Http\Controllers\ApiController@updateStudent');
+Route::delete('students/delete/{id}', 'App\Http\Controllers\ApiController@deleteStudent');
